f0f69ae7f7272f6809cfce05b2fde39e65386557
briannaordaz <briannaordaz5@gmail.com>
2026-02-11 17:58:06 -0800

    completed step 4, created new method with string and int parameters

--- COMP003A.LectureActivity5/Program.cs
+++ COMP003A.LectureActivity5/Program.cs
@@ -14,4 +14,11 @@ class Program
     {
         Console.WriteLine("Welcome to Module 5!");
     }
+
+    static void DisplayUserInfo(string name, int age)
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+    
+    
 }
